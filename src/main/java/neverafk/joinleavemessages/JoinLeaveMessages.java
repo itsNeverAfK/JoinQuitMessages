f920956cd655d303180a30eb91b9e7cd64f0d9a9
NeverAfK <52254539+itsNeverAfK@users.noreply.github.com>
2022-07-14 14:03:17 +0200

    Add files via upload

---
+++ src/main/java/neverafk/joinleavemessages/JoinLeaveMessages.java
@@ -0,0 +1,57 @@
+package neverafk.joinleavemessages;
+
+import org.bukkit.Bukkit;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
+import org.bukkit.plugin.Plugin;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.HashMap;
+
+public final class JoinLeaveMessages extends JavaPlugin implements Listener {
+
+    String joinMessage = null;
+    Boolean sendJoinMessage = null;
+
+    String quitMessage = null;
+    Boolean sendQuitMessage = null;
+
+    @Override
+    public void onEnable() {
+        Bukkit.getPluginManager().registerEvents(this, this);
+
+        this.saveDefaultConfig();
+
+        joinMessage = this.getConfig().getString("join.message");
+
+        if(this.getConfig().getString("join.sending").equals("true")) {
+            sendJoinMessage = true;
+        }
+
+        quitMessage = this.getConfig().getString("quit.message");
+
+        if(this.getConfig().getString("quit.sending").equals("true")) {
+            sendQuitMessage = true;
+        }
+    }
+
+    @EventHandler
+    public void onJoin(PlayerJoinEvent event) {
+        if(sendJoinMessage) {
+            event.setJoinMessage(joinMessage.replace("{ColorCode}", "§").replace("{player}", event.getPlayer().getName()).replace("{online}", String.valueOf(Bukkit.getOnlinePlayers().size())).replace("{max_online}", String.valueOf(Bukkit.getMaxPlayers())));
+        } else {
+            event.setJoinMessage(null);
+        }
+    }
+
+    @EventHandler
+    public void onQuit(PlayerQuitEvent event) {
+        if(sendQuitMessage) {
+            event.setQuitMessage(quitMessage.replace("{ColorCode}", "§").replace("{player}", event.getPlayer().getName()).replace("{online}", String.valueOf(Bukkit.getOnlinePlayers().size())).replace("{max_online}", String.valueOf(Bukkit.getMaxPlayers())));
+        } else {
+            event.setQuitMessage(null);
+        }
+    }
+}
